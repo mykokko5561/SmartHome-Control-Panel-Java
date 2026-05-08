3f41fd3b1584941f72ca7716e25b9fa91987f8b1
Mustafa Yavuz Karaoğlu <mykokko5561@gmail.com>
2026-05-08 22:05:27 +0300

    Reformat SicaklikDevice.java code structure

--- SicaklikDevice.java
+++ SicaklikDevice.java
@@ -1,8 +1,5 @@
 package ev_kontrol_20_proje;
 
-/**
- * @author Member 1
- */
 public abstract class SicaklikDevice extends Smartdevice {
     private int targetTemperature;
     private int currentTemperature;
