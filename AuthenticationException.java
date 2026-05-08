9d3bf8c451dc87c2c8236bbdced9d6357c9d3f03
Mustafa Yavuz Karaoğlu <mykokko5561@gmail.com>
2026-05-08 21:54:44 +0300

    Remove unnecessary comments and package declaration

--- AuthenticationException.java
+++ AuthenticationException.java
@@ -1,9 +1,5 @@
 package ev_kontrol_20_proje;
 
-/**
- * @author Umut
- * Kullanıcı giriş ve kayıt işlemlerinde oluşabilecek hatalar için özel Exception.
- */
 public class AuthenticationException extends Exception {
     public AuthenticationException(String message) {
         super(message);
