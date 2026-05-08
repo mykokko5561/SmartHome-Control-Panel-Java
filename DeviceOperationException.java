8976477f2ac05d969d3874f9905195bfb1b7fdbc
Mustafa Yavuz Karaoğlu <mykokko5561@gmail.com>
2026-05-08 21:57:45 +0300

    Update DeviceOperationException.java

--- DeviceOperationException.java
+++ DeviceOperationException.java
@@ -1,9 +1,5 @@
 package ev_kontrol_20_proje;
 
-/**
- * @author Umut
- * Cihaz operasyonlarında oluşabilecek hatalar için özel Exception sınıfı.
- */
 public class DeviceOperationException extends Exception {
     public DeviceOperationException(String message) {
         super(message);
