5fac534c8742f4aeaa529f3ae8ea077cb654ec12
Mustafa Yavuz Karaoğlu <mykokko5561@gmail.com>
2026-05-08 21:55:38 +0300

    Remove unnecessary lines and clean up CamasirMakinesi

--- CamasirMakinesi.java
+++ CamasirMakinesi.java
@@ -1,8 +1,5 @@
 package ev_kontrol_20_proje;
 
-/**
- * @author Member 1
- */
 public class CamasirMakinesi extends CleaningDevice {
     private int spinSpeed;
     private boolean isDetergentEmpty;
