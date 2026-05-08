0c80b2b56dbe8a276a63eb13d5c35bc0db0badbf
Mustafa Yavuz Karaoğlu <mykokko5561@gmail.com>
2026-05-08 22:08:36 +0300

    Update smartcamera.java

--- smartcamera.java
+++ smartcamera.java
@@ -1,8 +1,5 @@
 package ev_kontrol_20_proje;
 
-/**
- * @author Member 1
- */
 public class AkilliKamera extends SecurityDevice {
     private boolean isNightVisionOn;
     private boolean isMotionDetected;
